34dd769d7e61c6e1bf76a5d527278916eada4441
DNA-Dev <dna-dev@pop-os.localdomain>
2022-03-30 13:59:13 +0800

    added animation hero section

--- src/components/Scroller.tsx
+++ src/components/Scroller.tsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Scroller = () => {
+  const variants = {
+    hidden: { opacity: 0, y: 30 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 2,
+        type: "spring",
+        ease: "easeOut",
+      },
+    },
+  };
   return (
-    <div className="flex items-center absolute bottom-[100px]">
+    <motion.div
+      className="flex items-center absolute bottom-[100px]"
+      variants={variants}
+    >
       <div className="w-[18px] h-[40px] border-[1px] border-solid border-white relative rounded-[15px]">
         <div className="w-[2px] rounded-[8px] h-[12px] bg-white relative my-0 mx-auto animate-scroller"></div>
       </div>
-      <h4 className="text-gray-600 ml-4 animate-pulse">
-        Scroll
-      </h4>
-    </div>
+      <h4 className="text-gray-600 ml-4 animate-pulse">Scroll</h4>
+    </motion.div>
   );
 };
 
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,51 +1,50 @@
 import React from "react";
 import { AiOutlineTwitter } from "react-icons/ai";
-import {
-  FiGithub,
-  FiLinkedin,
-  FiFacebook,
-} from "react-icons/fi";
+import { FiGithub, FiLinkedin, FiFacebook } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 const Social = () => {
+  const variants = {
+    hidden: { opacity: 0, y: 30 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1.5,
+        type: "spring",
+        ease: "easeOut",
+      },
+    },
+  };
+
   let Icons = [
     {
-      icon: (
-        <AiOutlineTwitter className="group-hover:fill-white" />
-      ),
+      icon: <AiOutlineTwitter className="group-hover:fill-white" />,
       link: "/",
     },
     {
-      icon: (
-        <FiGithub className="group-hover:fill-white fill-black" />
-      ),
+      icon: <FiGithub className="group-hover:fill-white fill-black" />,
       link: "/",
     },
     {
-      icon: (
-        <FiLinkedin className="group-hover:fill-white fill-black" />
-      ),
+      icon: <FiLinkedin className="group-hover:fill-white fill-black" />,
       link: "/",
     },
     {
-      icon: (
-        <FiFacebook className="group-hover:fill-white fill-black" />
-      ),
+      icon: <FiFacebook className="group-hover:fill-white fill-black" />,
       link: "/",
     },
   ];
   return (
-    <div className="flex">
+    <motion.div className="flex" variants={variants}>
       {Icons.map((icon, index) => (
         <div className="w-[33.78px] h-[33.78px] rounded-full bg-[#928A97] flex items-center justify-center mr-3 cursor-pointer group hover:bg-sky-500">
-          <a
-            href={icon.link}
-            className="group-hover:text-white"
-          >
+          <a href={icon.link} className="group-hover:text-white">
             {icon.icon}
           </a>
         </div>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
